ca738173841ce61fdf02cc613ca5853adac1f16c
Gilson.Conceicao <gilson.conceicao@al.infnet.edu.br>
2022-12-17 16:43:33 -0300

    meta

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,6 @@ import * as gtag from "../lib/gtag";
 import Layout from "../components/layout/layout";
 import Head from "next/head";
 
-
 import "../styles/globals.scss";
 import "../styles/globals.css";
 
@@ -19,11 +18,6 @@ import "swiper/components/thumbs/thumbs.min.css";
 export function reportWebVitals(metric) {}
 
 const App = ({ Component, pageProps }) => {
-
-const  capa = '/media/capa.jpeg'
-  const useUser = { user: "User Nome", isAdmin: false };
-
- 
   const router = useRouter();
 
   useEffect(() => {
@@ -58,10 +52,30 @@ const  capa = '/media/capa.jpeg'
       <Layout>
         <Head>
 
-        <title>Gilson Paulo:  desenvolvedor web full-stack ReactJS,  NodeJS, VueJS e NuxtJS.</title>
-<meta name="title" content="Gilson Paulo:  desenvolvedor web full-stack ReactJS,  NodeJS, VueJS e NuxtJS."/>
-<meta name="description" content="Desenvolvedor front-end júnior atuando formalmente desde agosto de 2021. Front-end engineer working formally since August 2021."/>
+            {/* content="https://gilsonpaulo.com.br/android-chrome-192x192.png" */}
+ 
+<>
+{/* <!-- Primary Meta Tags --> */}
+<title>Olá, eu sou Gilson Paulo. Eu sou desenvolvedor web full-stack MERN + VueJS e NuxtJS.</title>
+<meta name="title" content="Gilson Paulo. Desenvolvedor full-stack MERN + VueJS e NuxtJS."/>
+<meta name="description" content="Desenvolvedor front-end atuando formalmente desde agosto de 2021. Front-end engineer working formally since August 2021."/>
+<meta name="author" content="Gilson Paulo"/>
+{/* <!-- Open Graph / Facebook --> */}
+<meta property="og:type" content="website"/>
+<meta property="og:url" content="https://gilsonpaulo.com.br/"/>
+<meta property="og:title" content="Gilson Paulo. Eu sou desenvolvedor web full-stack MERN + VueJS e NuxtJS."/>
+<meta property="og:description" content="Desenvolvedor front-end atuando formalmente desde agosto de 2021. Front-end engineer working formally since August 2021."/>
+<meta property="og:image" content="https://gilsonpaulo.com.br/android-chrome-192x192.png"/>
 
+{/* <!-- Twitter --> */}
+<meta property="twitter:card" content="summary_large_image"/>
+<meta property="twitter:url" content="https://gilsonpaulo.com.br/"/>
+<meta property="twitter:title" content="Gilson Paulo. Eu sou desenvolvedor web full-stack MERN + VueJS e NuxtJS."/>
+<meta property="twitter:description" content="Desenvolvedor front-end atuando formalmente desde agosto de 2021. Front-end engineer working formally since August 2021."/>
+<meta property="twitter:image" content="https://gilsonpaulo.com.br/android-chrome-192x192.png"/>
+</>
+
+<>
           <link
             rel="apple-touch-icon"
             sizes="180x180"
@@ -93,29 +107,7 @@ const  capa = '/media/capa.jpeg'
             href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;1,100&display=swap"
             rel="stylesheet"
           />
-          <meta property="og:type" content="website" />
-          <meta property="og:url" content="https://gilsonpaulo.com.br/" />
-          <meta
-            property="og:title"
-            content="Gilson Paulo:  desenvolvedor web full-stack ReactJS,  NodeJS, VueJS e NuxtJS."
-          />
-          <meta
-            property="og:description"
-            content="Desenvolvedor front-end júnior atuando formalmente desde agosto de 2021. Front-end engineer working formally since August 2021."
-          />
-          <meta property="og:image" content="https://gilsonpaulo.com.br/android-chrome-192x192.png" />
-          <meta property="twitter:card" content="summary_large_image" />
-          <meta property="twitter:url" content="https://gilsonpaulo.com.br/" />
-          <meta
-            property="twitter:title"
-            content="Gilson Paulo:  desenvolvedor web full-stack ReactJS,  NodeJS, VueJS e NuxtJS."
-          />
-          <meta
-            property="twitter:description"
-            content="Desenvolvedor front-end júnior atuando formalmente desde agosto de 2021. Front-end engineer working formally since August 2021."
-          />
-          <meta property="twitter:image" content="https://gilsonpaulo.com.br/android-chrome-192x192.png" />
-    
+</>
         </Head>
         <Component {...pageProps} />
       </Layout>
